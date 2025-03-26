6a67e21c7175306ca382438bb0a65a06bb1a6c93
SubhamMohanta <subhammohanta99@gmail.com>
2025-03-26 12:19:35 +0530

    removed headers in cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,12 +11,7 @@ const PORT = Number(process.env.PORT) || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/store';
 
 // ✅ Configure CORS Properly
-app.use(cors({
-  origin: ['https://product-web-app-nine.vercel.app/'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
